1411fdb2d0f0e859d16d22b17cbff62c48514433
Luis Alvarez Caballero <luisperso_015@hotmail.com>
2017-12-03 03:48:19 -0500

    Revert "FUNCIONA EL MAPA , VOY a llorar."
    This reverts commit 2091ca71c6fcdb6ae0cffcee760552b785bd1276.

--- AppMunicipalidadSantiago/AlertaSantiagoApp/AlertaSantiagoApp.Android/MainActivity.cs
+++ AppMunicipalidadSantiago/AlertaSantiagoApp/AlertaSantiagoApp.Android/MainActivity.cs
@@ -20,7 +20,6 @@ namespace AlertaSantiagoApp.Droid
             base.OnCreate(bundle);
             ButtonCircle.FormsPlugin.Droid.ButtonCircleRenderer.Init();//Button circle
             global::Xamarin.Forms.Forms.Init(this, bundle);
-            Xamarin.FormsMaps.Init(this, bundle);
             LoadApplication(new App());
         }
     }
